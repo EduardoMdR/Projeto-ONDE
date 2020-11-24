573a5e4abfd6faa2117f2a25dd2af3ab10afbbb1
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2020-11-24 13:51:57 -0300

    Creating pages
    Criando as páginas com seus métodos e configurando as rotas

--- WebSite/app/controllers/tests_controller.rb
+++ WebSite/app/controllers/tests_controller.rb
@@ -1,4 +1,36 @@
 class TestsController < ApplicationController
   def categorias
   end
+  def cupons
+  end
+  def ofertas
+  end
+  def contato
+  end
+  def parceiros
+  end
+  def empresas
+  end
+  def pesquisa
+  end
+  def ofertasdestaque
+  end
+
+  # empresas
+  def homeempresa
+  end
+  def criaroferta
+  end
+  def review
+  end
+  def dashboard
+  end
+  def todasofertas
+  end
+  def editardados
+  end
+
+  # admin
+  def setorempresas
+  end
 end
--- WebSite/config/routes.rb
+++ WebSite/config/routes.rb
@@ -4,5 +4,18 @@ Rails.application.routes.draw do
 
   scope 'test' do
     get 'categorias', to: "tests#categorias", as: :categorias
+    get 'cupons', to: "tests#cupons", as: :cupons
+    get 'ofertas', to: "tests#ofertas", as: :ofertas
+    get 'contato', to: "tests#contato", as: :contato
+    get 'parceiros', to: "tests#parceiros", as: :parceiros
+    get 'empresas', to: "tests#empresas", as: :empresas
+    get 'pesquisa', to: "tests#pesquisa", as: :pesquisa
+    get 'ofertasdestaque', to: "tests#ofertasdestaque", as: :ofertasdestaque
+    get 'homeempresa', to: "tests#homeempresa", as: :homeempresa
+    get 'criaroferta', to: "tests#criaroferta", as: :criaroferta
+    get 'review', to: "tests#review", as: :review
+    get 'dashboard', to: "tests#dashboard", as: :dashboard
+    get 'todasofertas', to: "tests#todasofertas", as: :todasofertas
+    get 'editardados', to: "tests#editardados", as: :editardados
   end
 end
