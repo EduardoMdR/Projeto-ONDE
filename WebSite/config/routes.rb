38b526d4c96d86bbe4c72baa05da7766c6fbb160
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2020-11-24 09:34:15 -0300

    Created Test Model
    Fiz uma model de Test para poder criar minhas rotas e minhas páginas (como vou fazer o css primeiro precisei fazer isso)

--- WebSite/config/routes.rb
+++ WebSite/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "application#homepage"
+
+  scope 'test' do
+    get 'categorias', to: "tests#categorias", as: :categorias
+  end
 end
