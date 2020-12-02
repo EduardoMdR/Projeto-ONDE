8c894301b3a077bedbf084d1f4492c237afd3f5a
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2020-12-02 11:20:43 -0300

    Dashboard
    * Foi adicionado uma página de controle (visualização das estatísticas) para a empresa

--- WebSite/app/assets/javascripts/chart.js
+++ WebSite/app/assets/javascripts/chart.js
@@ -45,3 +45,64 @@ $(document).on('turbolinks:load', async function () {
       }
   });
 });
+
+$(document).on('turbolinks:load', async function () { 
+  var ctx = document.getElementById('myChart2').getContext('2d');
+  var myChart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        labels: ['Cupons utilizados', 'Cupons não utilizados'],
+        datasets: [{
+            label: 'Total de visualizações',
+            data: [15, 19],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(153, 102, 255, 0.2)'
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(153, 102, 255, 1)'
+            ],
+            borderWidth: 1
+          }]
+      },
+
+  });
+});
+
+$(document).on('turbolinks:load', async function () { 
+  var ctx = document.getElementById('myChart3').getContext('2d');
+  var myChart = new Chart(ctx, {
+      type: 'horizontalBar',
+      data: {
+          labels: ['maça', 'perâ', 'pneu', 'michellin'],
+          datasets: [{
+              label: 'Prazo das ofertas',
+              data: [9, 8, 7.5, 6],
+              backgroundColor: [
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(54, 162, 235, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(54, 162, 235, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                    maxTicksLimit: 10,
+                    suggestedMax: 10
+                  }
+              }]
+          }
+      }
+  });
+});
