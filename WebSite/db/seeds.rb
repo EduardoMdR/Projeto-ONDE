551b126d69570dbec5afe49fd00f35f1a1c07567
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2020-12-08 21:06:32 -0300

    Close #1  Create Roles
    * Foi criado os tipos de usuários que poderão acessar o site (pessoas não logadas não contam)

--- WebSite/db/seeds.rb
+++ WebSite/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+already_seeded = Role.count.positive?
+
+unless already_seeded
+  # Criação das roles
+  adm_role = Role.create!(name: 'Administrador')
+  company_role = Role.create!(name: 'Empresa')
+  user_role = Role.create!(name: 'Usuário')
+
+  # Criação de usuários
+  # User.create(
+  #   email: 'sergio@enotecadecanterbsb.com.br',
+  #   password: '123456',
+  #   name: 'Sérgio Inoue',
+  #   cpf: '904.742.470-07',
+  #   phone_number: '(61) 99820-8800'
+  #   role: adm_role
+  # )
+end
